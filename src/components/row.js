4fcab4e71c8281389d58d822d047267945dd260f
grindcore819 <albert.villeneuve.nguyen@gmail.com>
2023-04-11 13:18:13 -0400

    Update row.js
    added the ALT in the images

--- src/components/row.js
+++ src/components/row.js
@@ -8,7 +8,7 @@ classMap.set("Visual", style.blue);
 export default function Row({title, result, icon}) {
     return(
         <div className={style.resultDashboardRow + " " +classMap.get(title)}>
-          <div><img src={icon}/>{title}</div>
+          <div><img src={icon} alt="{title}"/>{title}</div>
           <div><span className={style.result}>{result}</span><span className={style.denominator}> / 100</span></div>
         </div>
     )
